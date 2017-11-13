2b27095c85a0fefd45d6f5ced6ccee793a3497e4
cuishuxaing <307292568@qq.com>
2017-11-13 19:25:33 +0800

    添加  图片加载 进度使用  LoadingView 显示

--- app/src/main/java/com/mytestdemo/my_view/MyViewActivity.java
+++ app/src/main/java/com/mytestdemo/my_view/MyViewActivity.java
@@ -1,13 +1,25 @@
 package com.mytestdemo.my_view;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mytestdemo.R;
 import com.mytestdemo.view.MyCounterView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by cuishuxiang on 2017/3/31.
@@ -15,23 +27,30 @@ import com.mytestdemo.view.MyCounterView;
 
 public class MyViewActivity extends AppCompatActivity {
     private final String TAG = MyViewActivity.class.getSimpleName();
+    @BindView(R.id.spread_view)
+    CheckBox spreadView;
     private TextView mTextView;
     private Button mLeftButton;
     private Button mRightButon;
 
     private MyCounterView my_countview;
+
+    private ValueAnimator valueAnimator;
+    private ScaleAnimation scaleAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_layout);
+        ButterKnife.bind(this);
         init();
     }
 
-    private void init(){
-        mTextView=(TextView) findViewById(R.id.textView);
-        mLeftButton=(Button) findViewById(R.id.leftButton);
+    private void init() {
+        mTextView = (TextView) findViewById(R.id.textView);
+        mLeftButton = (Button) findViewById(R.id.leftButton);
         mLeftButton.setOnClickListener(new ClickListenerImpl());
-        mRightButon=(Button) findViewById(R.id.rightButton);
+        mRightButon = (Button) findViewById(R.id.rightButton);
         mRightButon.setOnClickListener(new ClickListenerImpl());
 
 
@@ -39,22 +58,36 @@ public class MyViewActivity extends AppCompatActivity {
          * 自己绘制了一个，点击 自增的view
          */
         my_countview = (MyCounterView) findViewById(R.id.my_countview);
+        spreadView = (CheckBox) findViewById(R.id.spread_view);
+        Log.d(TAG, "spreadView: " + spreadView.isChecked()+"  "+spreadView.getHeight());
+    }
+
+    @OnClick(R.id.spread_view)
+    public void onViewClicked() {
+        scaleAnimation = new ScaleAnimation(1f, 1.5f, 1f, 1.5f,
+                spreadView.getWidth() / 2, spreadView.getHeight() / 2);
+        scaleAnimation.setDuration(2000);
+
+        Log.d(TAG, "onViewClicked: " + spreadView.isChecked()+"  "+spreadView.getHeight());
+        if (spreadView.isChecked()){
+            spreadView.startAnimation(scaleAnimation);
+        }
 
     }
 
     /**
      * 示例说明:
      * 1 每次点击leftButton的时候
-     *   1.1 调用scrollBy()让mTextView的内容(即文字)在原本偏移的基础上往左移30
-     *   1.2 调用scrollBy()让mLeftButton的内容(即文字)在原本偏移的基础上也往左移30
+     * 1.1 调用scrollBy()让mTextView的内容(即文字)在原本偏移的基础上往左移30
+     * 1.2 调用scrollBy()让mLeftButton的内容(即文字)在原本偏移的基础上也往左移30
      * 2 每次点击rightButton的时候
-     *   2.1 调用scrollTo()让mTextView的内容(即文字)直接往右偏移30,而不管以前的基础(即 mScrollX和mScrollY)
+     * 2.1 调用scrollTo()让mTextView的内容(即文字)直接往右偏移30,而不管以前的基础(即 mScrollX和mScrollY)
      * 3 连续几次点击leftButton会看到mTextView的内容(即文字)每点一次都会往左移动30,
-     *   然后再点击一次rightButton会看到mTextView的内容(即文字)直接一次性到了往右30的位置,而
-     *   不是慢慢移动过去.
-     *   这么操作
-     *   1 很好的体现了这两个方法的区别.
-     *   2 直观地看了scrollTo()方法的效用,它是不管以前的偏移量的.
+     * 然后再点击一次rightButton会看到mTextView的内容(即文字)直接一次性到了往右30的位置,而
+     * 不是慢慢移动过去.
+     * 这么操作
+     * 1 很好的体现了这两个方法的区别.
+     * 2 直观地看了scrollTo()方法的效用,它是不管以前的偏移量的.
      * 4 在该例中也可以看到调用这两个方法时,View的背景是没有移动.移动的是内容.
      */
     private class ClickListenerImpl implements View.OnClickListener {
